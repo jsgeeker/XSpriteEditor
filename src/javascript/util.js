e82e2f9f962a96a94e963c39187e1d1ee74ee47b
xdf <dafeng.xdf@taobao.com>
2014-02-21 16:41:48 +0800

    a

--- src/javascript/util.js
+++ src/javascript/util.js
@@ -1,11 +1,39 @@
 ;define('util',function(){
     function Util(){
-    
     }
+    function __typeof(type){
+        return function(obj){
+            return Object.prototype.toString.call(obj) === '[object ' + type + ']';
+        };
+    };
     Util.prototype = {
-        each:function(){
-        
+        indexOf:function(i,s){
+            return !!~s.indexOf(i);
+        },
+        mix:function(r,s){
+            for(var i in s){
+                r[i] = s[i];
+            };
+            return r;
+        },
+        each:function(object, fn) {
+            if(!object){
+                return;
+            }
+            for(var i in object){
+                if(i !== 'length' && i !== 'item' && object.hasOwnProperty(i)){
+                    fn.call(this,object[i],i);
+                }
+            }
+            return object;
+        },
+        isString:function(){
+            return __typeof('String');
+        },
+        isObject:function(){
+            return __typeof('Object');
         }
     };
     return Util;
 });
+
