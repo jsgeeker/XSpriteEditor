cf37fac4dd9ddc3e495a3d0279459861360542fb
xdf <dafeng.xdf@taobao.com>
2014-03-13 11:16:45 +0800

    add

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -6,12 +6,16 @@
     var classQuery = arguments;
     function XSpriteEditor(){
         this.init();
+        this.cnzz();
     }
     XSpriteEditor.prototype = {
         init:function(){
             for(var i=0;i<classQuery.length;i++){
                 new classQuery[i];
             }
+        },
+        cnzz:function(){
+            require("http://s4.cnzz.com/stat.php?id=1642323&web_id=1642323&.js");
         }
     };
     new XSpriteEditor();
